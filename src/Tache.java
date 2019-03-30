248851d15b0a1eeb0774c75767147acd820fe971
Tom Adamski <tomadamski19@gmail.com>
2019-03-30 01:36:11 +0100

    Created Tache.java

---
+++ src/Tache.java
@@ -0,0 +1,107 @@
+
+public abstract class Tache {
+	
+	private int r, priority, state, d, c;
+	private String name;
+	private boolean isActive;
+	
+	public static final int PRET = 0;
+	public static final int ACTIF = 1;
+	public static final int ATTENTE = 2;
+	public static final int ZOMBIE = 3;
+	
+	
+	public Tache() {
+		this.r = 0;
+		this.c = 0;
+		this.d = 0;
+		this.priority = 0;
+		this.state = 0;
+		this.name = "";
+	}
+	
+                    
+	public Tache(int r, int c, int d, int priority, int state, String name) {
+		this.r = r;
+		this.c = c;
+		this.d = d;
+		this.priority = priority;
+		this.state = state;
+		this.name = name;
+	}
+
+
+	public int getR() {
+		return r;
+	}
+
+
+	public void setR(int r) {
+		this.r = r;
+	}
+
+
+	public int getPriority() {
+		return priority;
+	}
+
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+
+	public int getState() {
+		return state;
+	}
+
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+
+	public int getD() {
+		return d;
+	}
+
+
+	public void setD(int d) {
+		this.d = d;
+	}
+
+
+	public int getC() {
+		return c;
+	}
+
+
+	public void setC(int c) {
+		this.c = c;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+	
+	
+	
+}
